7f42d7526e971195813bb2731f490f45710b6064
Kenny Gourdier <kennethgourdier@gmail.com>
2025-03-08 12:13:39 -0500

    update sys args main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_num_words, get_chars_dict, get_sorted
+import sys
 
 def get_book_text(path):
     with open(path) as f :
@@ -6,7 +7,11 @@ def get_book_text(path):
         return file_contents
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv)<2:
+        print('Usage: python3 main.py <path_to_book>')
+        sys.exit(1)
+
+    book_path = sys.argv[1] 
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
